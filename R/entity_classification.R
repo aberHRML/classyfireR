df30b227094bce19cf08a1ea7ccc7c50cf36c768
wilsontom <wilsontom89@gmail.com>
2018-03-26 16:59:25 +0100

    re-write classification to catch missing fields

--- R/entity_classification.R
+++ R/entity_classification.R
@@ -1,57 +1,72 @@
 #' Entity Classification
 #'
+#' Retrive entity classification from `http://classyfire.wishartlab.com/entities/'
 #'
-#'
-#' @param INCHI_KEY a character string of a valid InChIKey
+#' @param inchi_key a character string of a valid InChIKey
 #' @return a list of two `tibbles`
 #'   - __ClassyFire__ a `tibble` containing the following;
-#'       - __Level__
-#'       - __Classification__
-#'       - __CHEMONT__
+#'       - __Level__ Classification level (kingdom, superclass, class and subclass)
+#'       - __Classification__ The compound classification
+#'       - __CHEMONT__ Chemical Ontology Identification code
 #'   - __Meta__ a `tibble` containing the following;
-#'       - __Query__
-#'       - __Version__
-#'       - __Date__
+#'       - __Query__ the submitted InChI key
+#'       - __Version__ Version of classyFire
+#'       - __Date__ Date classification retrieved
 #'
 #' @examples
 #' entity_classification('BRMWTNUJHUMWMS-LURJTMIESA-N')
 #'
+#' entity_classification('MDHYEMXUFSJLGV-UHFFFAOYSA-N')
+#'
 #' @export
-
-entity_classification <- function(INCHI_KEY)
+entity_classification <- function(inchi_key)
 {
   entity_url <- 'http://classyfire.wishartlab.com/entities/'
 
-  entity_query <- paste0(entity_url, INCHI_KEY, '.json')
+  entity_query <- paste0(entity_url, inchi_key, '.json')
 
   response <- httr::GET(entity_query)
 
   if (response$status_code == 404) {
-    message('No exisiting classification found', '/n')
-    message('Use `classyFire::classify` to create classification')
+    message(crayon::red(
+      clisymbols::symbol$cross,
+      'no exisiting classification available'
+    ))
+  }
 
-  } else{
+  if (response$status_code == 200) {
+    message(crayon::green(clisymbols::symbol$tick, 'classification retrieved'))
     text_content <- httr::content(response, 'text')
 
     json_res <- jsonlite::fromJSON(text_content)
 
-    entity_class <- list(
-      Kingdom = json_res$kingdom$name,
-      Superlcas = json_res$superclass$name,
-      Class = json_res$class$name,
-      Subclass = json_res$subclass$name
-    )
+    list_output <-
+      list(
+        kingdom = json_res[['kingdom']],
+        superclass = json_res[['superclass']],
+        class = json_res[['class']],
+        subclass = json_res[['subclass']]
+      )
 
-    entity_ontid <- list(
-      Kingdom = json_res$kingdom$chemont_id,
-      Superlcas = json_res$superclass$chemont_id,
-      Class = json_res$class$chemont_id,
-      Subclass = json_res$subclass$chemont_id
-    )
+    len <- purrr::map(list_output, length) %>% unlist()
 
-    entity_res <-
-      tibble::tibble(Level = names(entity_class),Classification = unlist(entity_class),
-                 CHEMONT = unlist(entity_ontid))
+    class_tibble <-
+      tibble::tibble(
+        Level = names(len),
+        Classification = 'NA',
+        CHEMONT = 'NA'
+      )
+
+    for (i in seq_along(len)) {
+      if (len[[i]] == 4) {
+        class_tibble[i, 'Classification'] <- list_output[[i]]$name
+        class_tibble[i, 'CHEMONT'] <- list_output[[i]]$chemont_id
+      } else{
+        class_tibble[i, 'Classification'] <- NA
+        class_tibble[i, 'CHEMONT'] <- NA
+      }
+    }
+
     entity_meta <-
       tibble::tibble(
         Query = json_res$inchikey,
@@ -59,6 +74,6 @@ entity_classification <- function(INCHI_KEY)
         Date = Sys.Date()
       )
 
-    return(list(ClassyFire = entity_res, Meta = entity_meta))
+    return(list(ClassyFire = class_tibble, Meta = entity_meta))
   }
 }
