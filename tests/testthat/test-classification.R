00bb7fa08d1ba98d33e3a6aabb845097c08ff619
wilsontom <wilsontom89@gmail.com>
2018-03-27 12:05:31 +0100

    first batch of simple tests

--- tests/testthat/test-classification.R
+++ tests/testthat/test-classification.R
@@ -1,5 +1,18 @@
 context('entity-classification')
 
-test_that('entity-classification',{
-}
-)
+test_that('entity-classification', {
+  expect_true(is.tbl(entity_classification('BRMWTNUJHUMWMS-LURJTMIESA-N')))
+
+  expect_true(is.tbl(entity_classification('MDHYEMXUFSJLGV-UHFFFAOYSA-N')))
+
+  expect_that(nrow(entity_classification('BRMWTNUJHUMWMS-LURJTMIESA-N')), equals(4))
+  expect_that(nrow(entity_classification('MDHYEMXUFSJLGV-UHFFFAOYSA-N')), equals(4))
+
+
+  expect_that(ncol(entity_classification('BRMWTNUJHUMWMS-LURJTMIESA-N')), equals(3))
+
+  expect_that(ncol(entity_classification('MDHYEMXUFSJLGV-UHFFFAOYSA-N')), equals(3))
+
+  expect_true(is.null(entity_classification('BRMWTNUJHUMWMS-LURJTMIESA-B')))
+
+})
