7d0f96716dba115077528aad2ba696fd316612a7
wilsontom <wilsontom89@gmail.com>
2018-03-27 12:09:33 +0100

    fix tests

--- tests/testthat/test-classification.R
+++ tests/testthat/test-classification.R
@@ -1,17 +1,17 @@
 context('entity-classification')
 
 test_that('entity-classification', {
-  expect_true(is.tbl(entity_classification('BRMWTNUJHUMWMS-LURJTMIESA-N')))
+  expect_true(dplyr::is.tbl(entity_classification('BRMWTNUJHUMWMS-LURJTMIESA-N')))
 
-  expect_true(is.tbl(entity_classification('MDHYEMXUFSJLGV-UHFFFAOYSA-N')))
+  expect_true(dplyr::is.tbl(entity_classification('MDHYEMXUFSJLGV-UHFFFAOYSA-N')))
 
-  expect_that(nrow(entity_classification('BRMWTNUJHUMWMS-LURJTMIESA-N')), equals(4))
-  expect_that(nrow(entity_classification('MDHYEMXUFSJLGV-UHFFFAOYSA-N')), equals(4))
+  expect_true(nrow(entity_classification('BRMWTNUJHUMWMS-LURJTMIESA-N')) == 4)
+  expect_true(nrow(entity_classification('MDHYEMXUFSJLGV-UHFFFAOYSA-N')) == 4)
 
 
-  expect_that(ncol(entity_classification('BRMWTNUJHUMWMS-LURJTMIESA-N')), equals(3))
+  expect_true(ncol(entity_classification('BRMWTNUJHUMWMS-LURJTMIESA-N')) == 3)
 
-  expect_that(ncol(entity_classification('MDHYEMXUFSJLGV-UHFFFAOYSA-N')), equals(3))
+  expect_true(ncol(entity_classification('MDHYEMXUFSJLGV-UHFFFAOYSA-N')) == 3)
 
   expect_true(is.null(entity_classification('BRMWTNUJHUMWMS-LURJTMIESA-B')))
 
