c332f2b3fe6c6b311c4d76803742699241e5b3c6
wilsontom <wilsontom89@gmail.com>
2018-03-27 10:42:53 +0100

    remove meta output and add wrap parse into function

--- R/entity_classification.R
+++ R/entity_classification.R
@@ -8,10 +8,6 @@
 #'       - __Level__ Classification level (kingdom, superclass, class and subclass)
 #'       - __Classification__ The compound classification
 #'       - __CHEMONT__ Chemical Ontology Identification code
-#'   - __Meta__ a `tibble` containing the following;
-#'       - __Query__ the submitted InChI key
-#'       - __Version__ Version of classyFire
-#'       - __Date__ Date classification retrieved
 #'
 #' @examples
 #' entity_classification('BRMWTNUJHUMWMS-LURJTMIESA-N')
@@ -40,42 +36,9 @@ entity_classification <- function(inchi_key)
 
     json_res <- jsonlite::fromJSON(text_content)
 
-
-    list_output <-
-      list(
-        kingdom = json_res[['kingdom']],
-        superclass = json_res[['superclass']],
-        class = json_res[['class']],
-        subclass = json_res[['subclass']]
-      )
-
-    len <- purrr::map(list_output, length) %>% unlist()
-
-    class_tibble <-
-      tibble::tibble(
-        Level = names(len),
-        Classification = 'NA',
-        CHEMONT = 'NA'
-      )
-
-    for (i in seq_along(len)) {
-      if (len[[i]] == 4) {
-        class_tibble[i, 'Classification'] <- list_output[[i]]$name
-        class_tibble[i, 'CHEMONT'] <- list_output[[i]]$chemont_id
-      } else{
-        class_tibble[i, 'Classification'] <- NA
-        class_tibble[i, 'CHEMONT'] <- NA
-      }
-    }
+    classification <- parse_json_output(json_res)
 
 
-    entity_meta <-
-      tibble::tibble(
-        Query = json_res$inchikey,
-        Version = json_res$classification_version,
-        Date = Sys.Date()
-      )
-
-    return(list(ClassyFire = class_tibble, Meta = entity_meta))
+    return(classification)
   }
 }
