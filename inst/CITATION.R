7bb1a572180507c225def086aa84581899f989c0
wilsontom <wilsontom89@gmail.com>
2018-06-13 12:30:28 +0100

    add package CITATION

---
+++ inst/CITATION.R
@@ -0,0 +1,17 @@
+citHeader('To cite classyfireR in publications please use:')
+
+citEntry(
+  entry = 'Article',
+  title = 'ClassyFire: Automated Chemical Classification With A Comprehensive, Computable Taxonomy',
+  author = personList(as.person('Djoumbou Feunang Y'), as.person('Eisner R'), as.person('Knox C'),
+                      as.person('Chepelev L'), as.person('Hastings J'), as.person('Owen G'),
+                      as.person('Fahy E'), as.person('Steinbeck C'), as.person('Subramanian S'),
+                      as.person('Bolton E'), as.person('Greiner R'), as.person('Wishart DS')),
+  journal = 'Journal of Cheminformatics',
+  volume = '8:61',
+  year = '2016',
+  url = 'https://doi.org/10.1186/s13321-016-0174-y',
+
+  textVersion = paste('Wishart et al; (2016). ClassyFire: Automated Chemical Classification With A Comprehensive, Computable Taxonomy.
+  Journal of Cheminformatics, 8:61')
+)
