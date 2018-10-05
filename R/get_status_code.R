3610602716b292b58242a9df001f72ea37646eaf
wilsontom <wilsontom89@gmail.com>
2018-10-05 09:45:36 +0100

    add example

--- R/get_status_code.R
+++ R/get_status_code.R
@@ -1,11 +1,13 @@
 #' Get Status Code
 #'
+#' Retreive the status code of a new submission for entity classification
+#'
 #' @param query_id a numeric value for the query id
 #' @return a list of `query_id` and classification status; either `In progress` or `Done`
+#'
 #' @export
 #' @examples
 #' get_status_code(2813259)
-
 
 get_status_code <- function(query_id)
 {
