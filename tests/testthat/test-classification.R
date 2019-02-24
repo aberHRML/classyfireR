1710ce8fb3606ce23d3a4e1bb67355244e9684bf
jasenfinch <jsf9@aber.ac.uk>
2019-02-24 23:46:07 +0000

    entity_classification now returns all classification levels

--- tests/testthat/test-classification.R
+++ tests/testthat/test-classification.R
@@ -5,8 +5,8 @@ test_that('entity-classification', {
 
   expect_true(dplyr::is.tbl(entity_classification('MDHYEMXUFSJLGV-UHFFFAOYSA-N')))
 
-  expect_true(nrow(entity_classification('BRMWTNUJHUMWMS-LURJTMIESA-N')) == 4)
-  expect_true(nrow(entity_classification('MDHYEMXUFSJLGV-UHFFFAOYSA-N')) == 4)
+  expect_true(nrow(entity_classification('BRMWTNUJHUMWMS-LURJTMIESA-N')) == 7)
+  expect_true(nrow(entity_classification('MDHYEMXUFSJLGV-UHFFFAOYSA-N')) == 3)
 
 
   expect_true(ncol(entity_classification('BRMWTNUJHUMWMS-LURJTMIESA-N')) == 3)
