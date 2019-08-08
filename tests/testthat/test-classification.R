c50e53536a500ece794e9f0abf6661da250eb68b
jasenfinch <jsf9@aber.ac.uk>
2019-08-08 16:25:04 +0100

    get_classification now errors if the request rate limit is exceeded. Fixed tests and checks

--- tests/testthat/test-classification.R
+++ tests/testthat/test-classification.R
@@ -1,17 +1,25 @@
 context('entity-classification')
 
 test_that('entity-classification', {
-  expect_true(dplyr::is.tbl(get_classification('BRMWTNUJHUMWMS-LURJTMIESA-N')))
 
-  expect_true(dplyr::is.tbl(get_classification('MDHYEMXUFSJLGV-UHFFFAOYSA-N')))
+  Sys.sleep(4)
+  cl1 <- get_classification('BRMWTNUJHUMWMS-LURJTMIESA-N')
+  Sys.sleep(4)
+  cl2 <- get_classification('MDHYEMXUFSJLGV-UHFFFAOYSA-N')
+  Sys.sleep(4)
+  cl3 <- get_classification('BRMWTNUJHUMWMS-LURJTMIESA-B')
 
-  expect_true(nrow(get_classification('BRMWTNUJHUMWMS-LURJTMIESA-N')) == 7)
-  expect_true(nrow(get_classification('MDHYEMXUFSJLGV-UHFFFAOYSA-N')) == 3)
+  expect_true(dplyr::is.tbl(cl1))
 
-  expect_true(ncol(get_classification('BRMWTNUJHUMWMS-LURJTMIESA-N')) == 3)
+  expect_true(dplyr::is.tbl(cl2))
 
-  expect_true(ncol(get_classification('MDHYEMXUFSJLGV-UHFFFAOYSA-N')) == 3)
+  expect_true(nrow(cl1) == 7)
+  expect_true(nrow(cl2) == 3)
 
-  expect_true(is.null(get_classification('BRMWTNUJHUMWMS-LURJTMIESA-B')))
+  expect_true(ncol(cl1) == 3)
+
+  expect_true(ncol(cl2) == 3)
+
+  expect_true(is.null(cl3))
 
 })
