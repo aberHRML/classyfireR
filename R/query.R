c8742d03cb3a473a4f5e3f27a10e65f0ed1be48e
gjgetzinger <gordon.getzinger@duke.edu>
2020-04-29 23:41:48 -0400

    closes #19. Adds functions for query by SMILES.
    Named lists of SMILES strings can be submitted for classification using POST, RETRY and GET. Results are read from json and retruned as a list.

---
+++ R/query.R
@@ -0,0 +1,72 @@
+#' Submits a ClassyFire query in a JSON format.
+#'
+#' @param label [String] the label of the query.
+#' @param input A named list or vector of SMILES strings.
+#' @param type [String] the type of the query.
+#'
+#' @return A list of tibbles named by input name.
+#' @export
+#'
+#' @examples
+#' input <- c(MOL1 = 'CCCOCC', MOL2 = 'COCC=CCC')
+#' submit_query(label = 'query_test', input = input, type = 'STRUCTURE')
+#'
+submit_query <- function(label, input, type = 'STRUCTURE'){
+  base_url <- 'http://classyfire.wishartlab.com/queries'
+  query_input <- paste(names(input), input, sep = '\t', collapse = '\n')
+  q <- rjson::toJSON(list(
+      label = label,
+      query_input = query_input,
+      query_type = 'STRUCTURE'
+    ))
+
+  resp <- httr::POST(
+    url = base_url,
+    body = q,
+    httr::content_type_json(),
+    httr::accept_json(),
+    httr::timeout(getOption('timeout'))
+  )
+
+  post_cont <- httr::content(resp)
+
+  url <- paste0(base_url, '/', post_cont$id, ".json")
+
+  resp <- httr::RETRY("GET",
+                      url = url,
+                      encode = "json",
+                      times = 100)
+
+  if (resp$status_code == 200) {
+    json_res <- get_query(query_id = post_cont$id, format = 'json')
+    classification <-
+      purrr::map(json_res$entities, parse_json_output)
+    out <- setNames(classification, sapply(json_res$entities, function(x){x$identifier}))
+  } else {
+    out <- NA
+  }
+  return(out)
+}
+
+
+#'Retrieves the classification results for a given query.
+#'
+#' @param query_id query_id [Integer] the ID of the query.
+#' @param format format [String] the format of the query (either JSON, CSV, or SDF)
+#'
+#' @return [Text] A text file displaying the classification results for
+#    the query's entities in the specified format.
+#' @export
+#'
+get_query <- function(query_id, format = c("json", "sdf", "csv")){
+  format <- match.arg(format, choices =  c("json", "sdf", "csv"), several.ok = F)
+  base_url <- 'http://classyfire.wishartlab.com/queries/'
+  url <- paste0(base_url, query_id, ".", format)
+  resp <- httr::GET(url = url, httr::accept_json())
+  if (resp$status_code == 200) {
+    cont <- httr::content(resp, 'parsed')
+  } else {
+    cont <- NA
+  }
+  return(cont)
+}
