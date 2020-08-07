d6d51de658b2b3414e43b81c2787d7c7b18e8221
wilsontom <wilsontom89@gmail.com>
2020-08-07 17:23:45 +0100

    parse query results using tidyjson

--- R/query.R
+++ R/query.R
@@ -12,14 +12,15 @@
 #' input <- c(MOL1 = 'CCCOCC', MOL2 = 'COCC=CCC')
 #' submit_query(label = 'query_test', input = input, type = 'STRUCTURE')
 #' }
-submit_query <- function(label, input, type = 'STRUCTURE'){
+submit_query <- function(label, input, type = 'STRUCTURE') {
   base_url <- 'http://classyfire.wishartlab.com/queries'
-  query_input <- paste(names(input), input, sep = '\t', collapse = '\n')
+  query_input <-
+    paste(names(input), input, sep = '\t', collapse = '\n')
   q <- rjson::toJSON(list(
-      label = label,
-      query_input = query_input,
-      query_type = 'STRUCTURE'
-    ))
+    label = label,
+    query_input = query_input,
+    query_type = 'STRUCTURE'
+  ))
 
   resp <- httr::POST(
     url = base_url,
@@ -40,13 +41,112 @@ submit_query <- function(label, input, type = 'STRUCTURE'){
 
   if (resp$status_code == 200) {
     json_res <- get_query(query_id = post_cont$id, format = 'json')
-    classification <-
-      purrr::map(json_res$entities, parse_json_output)
-    out <- stats::setNames(classification, sapply(json_res$entities, function(x){x$identifier}))
-  } else {
-    out <- NA
+
+    json_parse <- json_res %>%
+      tidyjson::enter_object(entities) %>%
+      tidyjson::gather_array() %>%
+      tidyjson::spread_all()
+
+
+    json_tib <- json_parse %>%
+      dplyr::as_tibble() %>%
+      dplyr::group_by(inchikey)
+
+    json_list <- json_parse$..JSON
+
+    object <- new('Query')
+
+    object@meta <-
+      json_tib %>% dplyr::select(identifier, inchikey, smiles, classification_version)
+
+
+    Classification <- json_tib %>%
+      dplyr::select(inchikey, dplyr::ends_with(".name")) %>%
+      tidyr::pivot_longer(-inchikey, values_to = "Classification") %>%
+      tidyr::separate(col = name,
+                      into = c("Level", "TYPE"),
+                      sep = "\\.") %>%
+      dplyr::select(-TYPE) %>% dplyr::ungroup() %>% dplyr::left_join(object@meta, ., by = 'inchikey') %>%
+      dplyr::select(-smiles,-classification_version)
+
+    object@classification <- Classification
+
+
+    CHEMONT <- json_tib %>%
+      dplyr::select(inchikey, dplyr::ends_with(".chemont_id")) %>%
+      tidyr::pivot_longer(-inchikey, values_to = "CHEMONT") %>%
+      tidyr::separate(col = name,
+                      into = c("Level", "TYPE"),
+                      sep = "\\.") %>%
+      dplyr::select(-TYPE)
+
+
+    DirectParents <- json_tib %>%
+      dplyr::select(inchikey, dplyr::starts_with("direct_parent.")) %>%
+      dplyr::rename(
+        name = direct_parent.name,
+        description = direct_parent.description,
+        chemont_id = direct_parent.chemont_id,
+        url = direct_parent.url
+      ) %>% dplyr::ungroup()
+
+
+    AltParents <- purrr::map(json_list, ~ {
+      .$alternative_parents
+    })
+
+
+
+    AltParents_List <- list()
+    for (i in seq_along(AltParents)) {
+      AltParents_List[[i]] <-
+        purrr::map(AltParents[[i]], dplyr::as_tibble) %>% dplyr::bind_rows() %>%
+        tibble::add_column(identifier = json_tib$identifier[i], .before = 'name')
+    }
+
+    object@alternative_parents <-
+      AltParents_List %>% dplyr::bind_rows()
+
+
+
+
+    PredChebi <- purrr::map(json_list, ~ {
+      as.character(.$predicted_chebi_terms)
+    })
+
+    names(PredChebi) <- object@meta$identifier
+
+    object@predicted_chebi <- PredChebi
+
+
+    Desc <- purrr::map(json_list, ~ {
+      .$description
+    })
+
+    names(Desc) <- object@meta$identifier
+
+    object@description <- Desc
+
+
+    object@classification <- Classification
+
+    object@direct_parent <-
+      DirectParents %>% dplyr::left_join(object@meta, ., by = 'inchikey') %>%
+      dplyr::select(-smiles, -classification_version)
+
+    object@alternative_parents <-
+      AltParents_List %>% dplyr::bind_rows()
+
+
+
+
+
+    class_tibble <- dplyr::left_join(Classification, CHEMONT) %>%
+      dplyr::ungroup()
+
+    return(object)
   }
-  return(out)
+
 }
 
 
@@ -59,13 +159,16 @@ submit_query <- function(label, input, type = 'STRUCTURE'){
 #    the query's entities in the specified format.
 #' @export
 #'
-get_query <- function(query_id, format = c("json", "sdf", "csv")){
-  format <- match.arg(format, choices =  c("json", "sdf", "csv"), several.ok = F)
+get_query <- function(query_id, format = c("json", "sdf", "csv")) {
+  format <-
+    match.arg(format,
+              choices =  c("json", "sdf", "csv"),
+              several.ok = F)
   base_url <- 'http://classyfire.wishartlab.com/queries/'
   url <- paste0(base_url, query_id, ".", format)
   resp <- httr::GET(url = url, httr::accept_json())
   if (resp$status_code == 200) {
-    cont <- httr::content(resp, 'parsed')
+    cont <- httr::content(resp, 'text')
   } else {
     cont <- NA
   }
