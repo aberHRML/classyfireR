842be89ffe962d5cf9771a6ae74cca958c2cf33d
wilsontom <wilsontom89@gmail.com>
2020-08-07 20:01:30 +0100

    global variables

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,20 @@
 #' @importFrom utils globalVariables
 
-globalVariables(c('.', 'Level', 'Classification'))
-
+globalVariables(
+  c(
+    '.',
+    'Level',
+    'Classification',
+    'entities',
+    'inchikey',
+    'identifier',
+    'smiles',
+    'classification_version',
+    'name',
+    'TYPE',
+    'direct_parent.name',
+    'direct_parent.description',
+    'direct_parent.chemont_id',
+    'direct_parent.url'
+  )
+)
